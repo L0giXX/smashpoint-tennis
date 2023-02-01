c1aba3c7450725571f5cf54e04f7981a54114a53
Marc Müller <marc.mueller04@gmail.com>
2023-02-01 22:14:29 +0100

    routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,19 @@
-import { useState } from "react";
+import Home from "./routes/home";
+import Mitgliedschaft from "./routes/mitgliedschaft";
+import Navbar from "./components/navbar";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/mitgliedschaft" element={<Mitgliedschaft />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
