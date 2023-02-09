2c8cab8b38373869cdfb0f53bca6dec797cc93ad
Marc Müller <marc.mueller04@gmail.com>
2023-02-09 14:02:40 +0100

    Update navbar.tsx

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -19,7 +19,7 @@ export default function Navbar() {
             <li>
               <a
                 href="/"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:active:text-blue-700"
               >
                 Home
               </a>
@@ -27,7 +27,7 @@ export default function Navbar() {
             <li>
               <a
                 href="/mitgliedschaft"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:active:text-blue-700"
               >
                 Mitgliedschaft
               </a>
